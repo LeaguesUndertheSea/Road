2518bcfe53dea61970539a9d52aaf9552d13fef0
Submarinetwentythousand <154326933+LeaguesUndertheSea@users.noreply.github.com>
2023-12-21 20:12:44 +0800

    Add files via upload

---
+++ demo_3/walk1.c
@@ -0,0 +1,21 @@
+////#include<stdio.h>
+////int g_val = 2023;
+////int main()
+////{
+////	printf("1:%d\n",g_val);
+////	printf("hehe\n");
+////	{
+////		printf("2:%d\n",g_val);
+////		int a = 10;
+////		printf("3:%d\n",g_val);
+////		printf("a = %d\n", a);
+////	}	return 0;
+////}
+////
+//#include<stdio.h>
+//extern int g_val;
+//int main()
+//{
+//	printf("%d\n",g_val);
+//	return 0;
+//}
---
+++ demo_3/walk2.c
@@ -0,0 +1,17 @@
+////#include<stdio.h>
+////int g_val = 2023;
+////int main()
+////{
+////	printf("%d\n",g_val);
+////	return 0;
+////}
+//#include<stdio.h>
+//{
+//	{
+//		int a = 10;
+//		printf("%d\n", a);
+//	}
+//	return 0;
+//}
+#define _CTR_SECURE_NO_WARNINGS
+int g_val = 2023;
